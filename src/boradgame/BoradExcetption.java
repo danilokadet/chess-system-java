33d26e42a5580b58322a53de9d72b1cc0a82967d
Danilos <daniloks2@gmail.com>
2020-04-23 10:34:17 -0300

    BorderException and defensive programing

---
+++ src/boradgame/BoradExcetption.java
@@ -0,0 +1,14 @@
+package boradgame;
+
+public class BoradExcetption extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	
+	public BoradExcetption(String msg) {
+		
+		super(msg);
+	}
+	
+
+}
