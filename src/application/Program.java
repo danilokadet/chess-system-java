d6c392f351b0b09ec9a45328fee291826a617c1c
Danilos <daniloks2@gmail.com>
2020-04-23 07:58:57 -0300

    First class position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boradgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 
+		Position position = new Position(3, 5);
+		
+		System.out.println(position);
 	}
 
 }
