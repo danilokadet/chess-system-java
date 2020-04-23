ea5098185ceadcac6906910c81fb7aadc382dba6
Danilos <daniloks2@gmail.com>
2020-04-23 08:13:44 -0300

    Start to implement Borad and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
+import boradgame.Board;
 import boradgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-
-		Position position = new Position(3, 5);
-		
-		System.out.println(position);
+		Board board = new Board(8, 8);
 	}
 
 }
